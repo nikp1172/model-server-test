70eadddf55ca95d9e5b1bfaa1b011d6d9556ff37
Nikhil Popli <l6u2h3d9u4z5z9s4@truefoundryworkspace.slack.com>
2023-05-25 12:05:14 +0530

    fix

--- tf-model/run.sh
+++ tf-model/run.sh
@@ -8,5 +8,5 @@ grep -E -v 'tfy-mlflow-client|mlfoundry' $REQ_TXT > .filtered_requirements.txt
 pip install -U pip
 echo "Installing model pip package requirements:-"
 cat .filtered_requirements.txt
-pip install -r --use-deprecated=legacy-resolver .filtered_requirements.txt mlflow==1.26.0 mlserver==1.1.0 mlserver-mlflow==1.1.0 "protobuf<=3.20.0"
+pip install --use-deprecated=legacy-resolver -r .filtered_requirements.txt mlflow==1.26.0 mlserver==1.1.0 mlserver-mlflow==1.1.0 "protobuf<=3.20.0"
 mlserver start $MODELS_DIR
